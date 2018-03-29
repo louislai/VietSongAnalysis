7fd323fd157a56b7101980655d9556e5e79cfa31
Bui Do Hiep <hiep.bui@thecarousell.com>
2018-03-29 12:16:12 +0700

    Implement get lyric and do print to file for crawler

--- crawler/main.py
+++ crawler/main.py
@@ -6,12 +6,11 @@ import re
 
 class Song:
     def __init__(self, name, artist, lyric):
-        self.name = name
-        self.artist = artist
-        self.lyric = lyric
+        self.name = name.strip().lower()
+        self.artist = artist.strip().lower()
+        self.lyric = lyric.strip().lower()
 
     def fix_name(self, name):
-        name = name.strip()
         name = re.sub('- REMIX$', '', name)
         return name.strip()
 
@@ -22,8 +21,9 @@ class Song:
 
 
 class SongCrawler:
-    def __init__(self, page_url):
+    def __init__(self, page_url, counter):
         self.page_url = page_url
+        self.counter = counter
 
     def get_page(self, link):
         headers = {
@@ -32,25 +32,46 @@ class SongCrawler:
 
     def start_crawl(self):
         links = self.get_links_from_page(self.page_url)
+        for link in links:
+            song = self.get_song_from_link(link)
+            self.counter += 1
+            print(self.counter)
+            self.write_to_file(song, "../data/{}.txt".format(self.counter))
+
+    def write_to_file(self, song, filename):
+        file = open(filename, "w")
+        file.write(str(song))
+        file.close()
 
     def get_links_from_page(self, page_url):
-        print("start get links from page ----------", page_url)
-
         page = self.get_page(page_url)
         document = html.fromstring(page.text)
         links = document.xpath('//h2[@class="name"]/a/@href')
 
         return links
 
-    def get_lyric_from_link(self, link):
-        print("...")
+    def get_song_from_link(self, link):
+        page = self.get_page(link)
+        document = html.fromstring(page.text)
+        name = document.xpath('//h2[@class="name"]/text()')[0]
+        artist = document.xpath('//div[@class="artist"]/a/text()')[0]
+        lyric = document.xpath('//div[@class="lyric"]')[0].text_content()
+        return Song(name, artist, lyric)
 
 
 def main():
-    print("starting:... ")
-    crawler = SongCrawler("http://chonbaihat.com/karaoke-arirang/trang/797")
-    crawler.start_crawl()
-    print("finish:... ")
+    print("Start")
+
+    page_num = 797
+    couter = 0
+
+    for idx in range(1, page_num + 1):
+        crawler = SongCrawler(
+            "http://chonbaihat.com/karaoke-arirang/trang/{}".format(idx), couter)
+        crawler.start_crawl()
+        couter = crawler.counter
+
+    print("Finish")
 
 
 if __name__ == '__main__':
